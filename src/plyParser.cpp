f2a242196750657559184272b0dac5f7ba805fce
Masonlet <masonletoile@hotmail.com>
2025-10-01 22:00:48 -0400

    Reorganize graphics

--- src/plyParser.cpp
+++ src/plyParser.cpp
@@ -1,7 +1,7 @@
 #include "StarletParser/parser.hpp"
 #include "StarletParser/utils/log.hpp"
 
-#include "StarletGraphics/mesh/mesh.hpp"
+#include "StarletGraphics/resource/mesh.hpp"
 #include "StarletMath/constants.hpp"
 
 #include <string>
